b8cd838c3b7134ba946a9afd027ba782918a00ca
Kwanghoon Son <kwangson@yahoo.com>
2023-04-07 22:08:18 +0900

    mail send function

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import yfinance as yf
+import smtplib
 
-with open('kospi.txt') as f:
-    lines = [line.strip() for line in f]
+def send_email(user, pwd, recipient, subject, body):
+    
 
-lines.pop(0)
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
 
-for code in lines:
-    ticker = yf.Ticker(code + ".ks")
-    if (ticker.info['fiftyTwoWeekLowChangePercent'] < 0.05):
-        print('symbol : {}, shortName : {}, 52w_low_% : {}'.format(
-            ticker.info['symbol'], ticker.info['shortName'], ticker.info['fiftyTwoWeekLowChangePercent']))
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except:
+        print("failed to send mail")
+
+send_email('', '', '', 'my first test', 'hi works?')
+# with open('kospi.txt') as f:
+#     lines = [line.strip() for line in f]
+
+# lines.pop(0)
+
+# message = ""
+# cnt = 0
+
+# for code in lines:
+#     ticker = yf.Ticker(code + ".ks")
+#     if (ticker.info['fiftyTwoWeekLowChangePercent'] < 0.05):
+        
+#         message += ('symbol : {}, shortName : {}, 52w_low_% : {}'.format(
+#             ticker.info['symbol'], ticker.info['shortName'], ticker.info['fiftyTwoWeekLowChangePercent'])) + '\n'
+#         cnt += 1
+#         if(cnt == 2):
+#             break
+
+# print(message)
