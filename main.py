5026268368bc07abc264e5ccc55127e52fd37a68
Kwanghoon Son <kwangson@yahoo.com>
2023-04-10 18:28:50 +0900

    fix bug

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import yfinance as yf
 import smtplib
+import json
+from datetime import date
+
 
 def send_email(user, pwd, recipient, subject, body):
-    
+
+    if __debug__:
+        print('-----mail debug------')
+        print('{}\n{}'.format(subject, body))
+        print('---------------------')
+        return
 
     FROM = user
     TO = recipient if isinstance(recipient, list) else [recipient]
@@ -23,23 +31,31 @@ def send_email(user, pwd, recipient, subject, body):
     except:
         print("failed to send mail")
 
-send_email('', '', '', 'my first test', 'hi works?')
-# with open('kospi.txt') as f:
-#     lines = [line.strip() for line in f]
 
-# lines.pop(0)
+with open('kospi.txt') as f:
+    lines = [line.strip() for line in f]
 
-# message = ""
-# cnt = 0
+lines.pop(0)
 
-# for code in lines:
-#     ticker = yf.Ticker(code + ".ks")
-#     if (ticker.info['fiftyTwoWeekLowChangePercent'] < 0.05):
-        
-#         message += ('symbol : {}, shortName : {}, 52w_low_% : {}'.format(
-#             ticker.info['symbol'], ticker.info['shortName'], ticker.info['fiftyTwoWeekLowChangePercent'])) + '\n'
-#         cnt += 1
-#         if(cnt == 2):
-#             break
+message = ""
+cnt = 0
 
-# print(message)
+for code in lines:
+    ticker = yf.Ticker(code + ".ks")
+    if not 'fiftyTwoWeekLowChangePercent' in ticker.info:
+        print(ticker.info)
+        continue
+
+    if (ticker.info['fiftyTwoWeekLowChangePercent'] < 0.03):
+        message += ('symbol : {}, shortName : {}, 52w_low_% : {}'.format(
+            ticker.info['symbol'], ticker.info['shortName'], ticker.info['fiftyTwoWeekLowChangePercent'])) + '\n'
+        cnt += 1
+        if (cnt == 10):
+            break
+
+with open(".secret", "r") as json_file:
+    secret_data = json.load(json_file)
+
+mail_header = "Report " + str(date.today())
+send_email(secret_data['mail_id'], secret_data['mail_key'],
+           secret_data['recipient'], mail_header, message)
